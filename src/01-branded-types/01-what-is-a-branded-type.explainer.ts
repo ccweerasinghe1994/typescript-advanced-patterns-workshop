e9337be8c7b82085d605825821943289ec2175d6
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-15 09:14:30 +0530

    feat: initial commit

---
+++ src/01-branded-types/01-what-is-a-branded-type.explainer.ts
@@ -0,0 +1,29 @@
+import { Brand } from '../helpers/Brand';
+
+type Password = Brand<string, 'Password'>;
+type Email = Brand<string, 'Email'>;
+
+type UserObject = Brand<
+	{
+		id: string;
+		name: string;
+	},
+	'User'
+>;
+
+const user: UserObject = {
+	id: 'awdawd',
+	name: 'awdawdawd'
+} as UserObject;
+
+const verifyPassword = (password: Password) => {};
+
+verifyPassword('awdawdawd' as Password);
+
+const password = '1231423' as Password;
+
+const email = 'mpocock@me.com' as Email;
+
+let passwordSlot: Password;
+
+passwordSlot = 'awdjhawdjhbawd' as Password;
