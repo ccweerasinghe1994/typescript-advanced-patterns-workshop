0b4fd26f240787b6a0703d8cd115fae677c0ed01
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-04-16 12:03:29 +0530

    intial commit

--- src/01-branded-types/05-currency-conversion.problem.ts
+++ src/01-branded-types/05-currency-conversion.problem.ts
@@ -1,74 +1,85 @@
-import { describe, it } from "vitest";
-import { Brand } from "../helpers/Brand";
+import { describe, it } from 'vitest';
+import { Brand } from '../helpers/Brand';
+import { V } from 'vitest/dist/global-60f880c6';
+
+type ValidAmount = Brand<number, 'ValidAmount'>;
+type AuthorizedUser = Brand<User, 'AuthorizedUser'>;
 
 interface User {
-  id: string;
-  name: string;
-  maxConversionAmount: number;
+	id: string;
+	name: string;
+	maxConversionAmount: number;
 }
 
 // Mocks a function that uses an API to convert
 // One currency to another
 const getConversionRateFromApi = async (
-  amount: number,
-  from: string,
-  to: string,
+	amount: number,
+	from: string,
+	to: string
 ) => {
-  return Promise.resolve(amount * 0.82);
+	return Promise.resolve((amount * 0.82) as ValidAmount);
 };
 
 // Mocks a function which actually performs the conversion
-const performConversion = async (user: User, to: string, amount: number) => {};
+const performConversion = async (
+	user: AuthorizedUser,
+	to: string,
+	amount: ValidAmount
+) => {};
 
-const ensureUserCanConvert = (user: User, amount: number): User => {
-  if (user.maxConversionAmount < amount) {
-    throw new Error("User cannot convert currency");
-  }
+const ensureUserCanConvert = (
+	user: User,
+	amount: ValidAmount
+): AuthorizedUser => {
+	if (user.maxConversionAmount < amount) {
+		throw new Error('User cannot convert currency');
+	}
 
-  return user;
+	return user as AuthorizedUser;
 };
 
-describe("Possible implementations", () => {
-  it("Should error if you do not authorize the user first", () => {
-    const handleConversionRequest = async (
-      user: User,
-      from: string,
-      to: string,
-      amount: number,
-    ) => {
-      const convertedAmount = await getConversionRateFromApi(amount, from, to);
+describe('Possible implementations', () => {
+	it('Should error if you do not authorize the user first', () => {
+		const handleConversionRequest = async (
+			user: User,
+			from: string,
+			to: string,
+			amount: ValidAmount
+		) => {
+			const convertedAmount = await getConversionRateFromApi(amount, from, to);
 
-      // @ts-expect-error
-      await performConversion(user, to, convertedAmount);
-    };
-  });
+			// @ts-expect-error
+			await performConversion(user, to, convertedAmount);
+		};
+	});
 
-  it("Should error if you do not convert the amount first", () => {
-    const handleConversionRequest = async (
-      user: User,
-      from: string,
-      to: string,
-      amount: number,
-    ) => {
-      // @ts-expect-error
-      const authorizedUser = ensureUserCanConvert(user, amount);
+	it('Should error if you do not convert the amount first', () => {
+		const handleConversionRequest = async (
+			user: User,
+			from: string,
+			to: string,
+			amount: number
+		) => {
+			// @ts-expect-error
+			const authorizedUser = ensureUserCanConvert(user, amount);
 
-      // @ts-expect-error
-      await performConversion(authorizedUser, to, amount);
-    };
-  });
+			// @ts-expect-error
+			await performConversion(authorizedUser, to, amount);
+		};
+	});
 
-  it("Should pass type checking if you authorize the user AND convert the amount", () => {
-    const handleConversionRequest = async (
-      user: User,
-      from: string,
-      to: string,
-      amount: number,
-    ) => {
-      const convertedAmount = await getConversionRateFromApi(amount, from, to);
-      const authorizedUser = ensureUserCanConvert(user, convertedAmount);
+	it('Should pass type checking if you authorize the user AND convert the amount', () => {
+		const handleConversionRequest = async (
+			user: User,
+			from: string,
+			to: string,
+			amount: number
+		) => {
+			const convertedAmount = await getConversionRateFromApi(amount, from, to);
+			const authorizedUser = ensureUserCanConvert(user, convertedAmount);
 
-      await performConversion(authorizedUser, to, convertedAmount);
-    };
-  });
+			await performConversion(authorizedUser, to, convertedAmount);
+		};
+	});
 });
